d3d59ed551c8339e9c4d217d21355373b0ef991c
pbagastama <bagas@microad.co.id>
2020-04-15 08:42:15 +0700

    update tweenmax

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
-// Animation GSAP
+// Header Banner
 TweenMax.to(".overlay img", 2, {
     duration: 3,
     opacity: 0,
@@ -26,7 +26,7 @@ TweenMax.to(".overlay span", 2, {
 TweenMax.to(".overlay", 2, {
     duration: 10.5,
     delay: 1,
-    top: "-100%",
+    top: -2000,
     ease: Expo.easeInOut
 })
 
@@ -84,40 +84,3 @@ TweenMax.from(".star-icon", 1, {
     opacity: 0,
     ease: Expo.easeInOut
 })
-
-// const lineOne = {
-//     curviness: 1.5,
-//     autoRotate: false,
-//     values: [
-//         {
-//             x: 0,
-//             y: 100
-//         },
-//         {
-//             x: 100,
-//             y: -50
-//         } 
-//     ]
-
-// };
-
-// const tween = new TimelineLite();
-
-// tween.add(
-//     TweenLite.to(".icon-hi", 1, {
-//         bezier: lineOne,
-//         ease: Power1.easeInOut
-//     })
-// )
-
-// const controller = new ScrollMagic.Controller();
-
-// const scene = new ScrollMagic.Scene({
-//     trigerElement: ".hi-kidjo",
-//     duration: 3000,
-//     trigerHook: 0
-// })
-// .setTween(tween)
-// .addIndicators()
-// .setPin('.hi-kidjo')
-// .addTo(controller);
